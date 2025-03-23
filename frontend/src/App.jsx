9d9283aceedfeb7cfb30e1572b1154cdec41e1e7
Luciano Bermudez <lucianobmecheng69@gmail.com>
2025-03-23 12:27:07 -0700

    added sqlite database for question embeddings

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+// App.js
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import ModuleTable from './components/Modules';
+import Navbar from './components/NavBar';
+import Home from './components/Home';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/modules" element={<ModuleTable />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
